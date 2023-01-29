9e03b097d2d503d220a5ab96f30fbfd08bf918df
Blaz Marinic <blaz.marinic@gmail.com>
2023-01-29 15:30:17 +0100

    fixed task team members display

--- TeamWorkApp/Models/TaskStatus.cs
+++ TeamWorkApp/Models/TaskStatus.cs
@@ -1,9 +1,14 @@
-﻿namespace TeamWorkApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamWorkApp.Models
 {
     public enum TaskStatus
     {
+        [Display(Name = "Novo")]
         New,
+        [Display(Name = "V teku")]
         InProgress,
+        [Display(Name = "Zaključeno")]
         Completed
     }
 }
