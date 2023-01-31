12d2ff8bcb4f7238d736f075def0ff22f4761a5e
Blaz Marinic <blaz.marinic@gmail.com>
2023-01-31 08:41:09 +0100

    missing migrations

---
+++ TeamWorkApp/Migrations/202301291344404_a.cs
@@ -0,0 +1,56 @@
+﻿namespace TeamWorkApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class a : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        TaskID = c.Int(nullable: false, identity: true),
+                        TaskDescription = c.String(),
+                        TaskDueDate = c.DateTime(nullable: false),
+                        TaskStatus = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TaskID);
+            
+            CreateTable(
+                "dbo.TeamMembers",
+                c => new
+                    {
+                        TeamMemberID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.TeamMemberID);
+            
+            CreateTable(
+                "dbo.TeamMemberTasks",
+                c => new
+                    {
+                        TeamMember_TeamMemberID = c.Int(nullable: false),
+                        Task_TaskID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.TeamMember_TeamMemberID, t.Task_TaskID })
+                .ForeignKey("dbo.TeamMembers", t => t.TeamMember_TeamMemberID, cascadeDelete: true)
+                .ForeignKey("dbo.Tasks", t => t.Task_TaskID, cascadeDelete: true)
+                .Index(t => t.TeamMember_TeamMemberID)
+                .Index(t => t.Task_TaskID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TeamMemberTasks", "Task_TaskID", "dbo.Tasks");
+            DropForeignKey("dbo.TeamMemberTasks", "TeamMember_TeamMemberID", "dbo.TeamMembers");
+            DropIndex("dbo.TeamMemberTasks", new[] { "Task_TaskID" });
+            DropIndex("dbo.TeamMemberTasks", new[] { "TeamMember_TeamMemberID" });
+            DropTable("dbo.TeamMemberTasks");
+            DropTable("dbo.TeamMembers");
+            DropTable("dbo.Tasks");
+        }
+    }
+}
---
+++ TeamWorkApp/Migrations/202301291351315_b.cs
@@ -0,0 +1,30 @@
+﻿namespace TeamWorkApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class b : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.TeamMemberTasks", newName: "TeamMemberTask");
+            RenameColumn(table: "dbo.TeamMemberTask", name: "TeamMember_TeamMemberID", newName: "TaskRefId");
+            RenameColumn(table: "dbo.TeamMemberTask", name: "Task_TaskID", newName: "TeamMemberRefId");
+            RenameIndex(table: "dbo.TeamMemberTask", name: "IX_Task_TaskID", newName: "IX_TeamMemberRefId");
+            RenameIndex(table: "dbo.TeamMemberTask", name: "IX_TeamMember_TeamMemberID", newName: "IX_TaskRefId");
+            DropPrimaryKey("dbo.TeamMemberTask");
+            AddPrimaryKey("dbo.TeamMemberTask", new[] { "TeamMemberRefId", "TaskRefId" });
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.TeamMemberTask");
+            AddPrimaryKey("dbo.TeamMemberTask", new[] { "TeamMember_TeamMemberID", "Task_TaskID" });
+            RenameIndex(table: "dbo.TeamMemberTask", name: "IX_TaskRefId", newName: "IX_TeamMember_TeamMemberID");
+            RenameIndex(table: "dbo.TeamMemberTask", name: "IX_TeamMemberRefId", newName: "IX_Task_TaskID");
+            RenameColumn(table: "dbo.TeamMemberTask", name: "TeamMemberRefId", newName: "Task_TaskID");
+            RenameColumn(table: "dbo.TeamMemberTask", name: "TaskRefId", newName: "TeamMember_TeamMemberID");
+            RenameTable(name: "dbo.TeamMemberTask", newName: "TeamMemberTasks");
+        }
+    }
+}
